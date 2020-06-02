077493e3289956f9d04dbafd75e8d7da54be1c6e
akumachan <akumachanit@gmail.com>
2020-06-02 22:44:39 +0900

    mken修正
    記事名にスペースを入れられない問題修正 -> 引数には_を代わりに入れるru

--- mken.sh
+++ mken.sh
@@ -3,13 +3,13 @@
 #Parameter1: Category name
 #Parameter2: Entry title
 
-dir="./$1/$2/"
+dir="./${1}/${2}/"
 file="${dir}index.md"
 
 cd ./content/blog/
 mkdir -p $dir
 echo "---" > ${file}
-echo "title: ${2}" >> ${file}
+echo "title: ${2//_/ }" >> ${file}
 echo "date: \"`date "+%Y-%m-%d %H:%M:%S"`\"" >> ${file}
 echo "description: \"\"" >> ${file}
 echo "category: \"${1}\"" >> ${file}
